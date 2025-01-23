17df918647b1f793a7d50b72406f70f89a5a9ac9
Mohamed Riza <mohamedriza94@gmail.com>
2025-01-23 16:49:30 +0530

    update

--- src/shared/utils/show-default-image.ts
+++ src/shared/utils/show-default-image.ts
@@ -1,5 +1,9 @@
 import { TImage } from "../types/image";
 
 export const showDefaultImage = (images: TImage[]) => {
-  return images?.find((image) => image.isDefault)?.url || images[0].url;
+  return (
+    images?.find((image) => image.isDefault)?.url ||
+    images[0]?.url ||
+    "http://res.cloudinary.com/dziodufj8/image/upload/v1736911344/temp/swygljxyvb8muitbezwg.png"
+  );
 };
